b02a2583c6a81e2d647e262adacb884ed8657bee
raselrostock <raselrostock@gmail.com>
2018-07-24 14:49:24 +0300

    Set file to push hash into Ethereum Blockchain

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
-
+import ipfs from './ipfs'
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
@@ -12,9 +12,13 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
-      web3: null
+      ipfsHash: '',
+      web3: null,
+      buffer: null,
+      account: null
     }
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -48,41 +52,64 @@ class App extends Component {
     simpleStorage.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
+        this.simpleStorageInstance = instance
+        this.setState({ account: accounts[0]})
+        return this.simpleStorageInstance.get.call(accounts[0])
+      }).then((ipfsHash) => {
         // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
+        return this.setState({ ipfsHash })
       })
     })
   }
+  captureFile(event){
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () =>{
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log(this.state.buffer);
+    }
+  }
+  onSubmit(event){
+    event.preventDefault();
+    ipfs.files.add(this.state.buffer, (err, result) =>{
+      if(err)
+      {
+        console.log(err);
+        return;
+      }
+      this.simpleStorageInstance.set(result[0].hash, { from: this.state.account })
+      .then((res)=>{
+        console.log(result[0].hash);
+        return this.setState({ ipfsHash: result[0].hash });
+      })
+    })
+  }
+
 
   render() {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">DApp- IPFS File Uploader</a>
         </nav>
 
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h1>Your Image</h1>
+              <p>This image stored on IPFS & Ethereum Blockchain.</p>
+              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
+              <h2>Upload Image </h2>
+              <form onSubmit={ this.onSubmit } >
+                <input type="file" onChange={ this.captureFile } />
+                <input type="submit" />
+              </form>
             </div>
           </div>
         </main>
